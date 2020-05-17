c96d61e70c07d5325577b18e0639228ec1bacc95
marcialmiguelrojas@gmail.com <w1gu3lr0j45m4rc1@gmail.com>
2020-05-17 01:24:05 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOCUMENTO</title>
+</head>
+<body>
+    <H1>hOLA MUNDO</H1>
+</body>
+</html>
